07e64f2f350bfa1a219f883cf9f5a0396bd1cd9f
Abarna Shanmugam <98462784+abarna-24@users.noreply.github.com>
2023-06-24 19:22:20 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\flutter_windows_3.7.6-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\flutter_project\\shop_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\flutter_windows_3.7.6-stable\\flutter"
+  "PROJECT_DIR=D:\\flutter_project\\shop_app"
+  "FLUTTER_ROOT=D:\\flutter_windows_3.7.6-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\flutter_project\\shop_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\flutter_project\\shop_app"
+  "FLUTTER_TARGET=D:\\flutter_project\\shop_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\flutter_project\\shop_app\\.dart_tool\\package_config.json"
+)
